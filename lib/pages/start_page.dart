aa72f68bac605ba3a85cdb37c9614e51be639931
Saad Mulla <saadnmulla@gmail.com>
2022-04-20 18:58:42 +0530

    fixed

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -8,63 +8,68 @@ class StartPage extends StatelessWidget {
     return SafeArea(
       child: Material(
         color: Colors.orange[200],
-        child: Column(
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            Image.asset(
-              "assets/img/start_page.jpg",
-              fit: BoxFit.contain,
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Text(
-              "Welcome!",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30,
               ),
-            ),
-            SizedBox(
-              height: 100,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, MyRoutes.signupRoute);
-              },
-              child: Text(
-                "Sign up",
+              Image.asset(
+                "assets/img/start_page.jpg",
+                fit: BoxFit.contain,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Text(
+                "Welcome!",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              style: ElevatedButton.styleFrom(
-                  minimumSize: Size(150, 50), primary: Colors.black),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, MyRoutes.loginRoute);
-              },
-              child: Text(
-                "Log in",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
+              SizedBox(
+                height: 100,
               ),
-              style: ElevatedButton.styleFrom(
-                  minimumSize: Size(150, 50), primary: Colors.black),
-            ),
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, MyRoutes.signupRoute);
+                },
+                child: Text(
+                  "Sign up",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size(150, 50), primary: Colors.black),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, MyRoutes.loginRoute);
+                },
+                child: Text(
+                  "Log in",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size(150, 50), primary: Colors.black),
+              ),
+              SizedBox(
+                height: 40,
+              )
+            ],
+          ),
         ),
       ),
     );
